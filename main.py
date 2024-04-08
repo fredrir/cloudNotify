5820a35ff9c7e84ced79e66c2f54cb4ce83d4054
fredrir <fhansteen@gmail.com>
2024-04-08 18:43:20 +0200

    fixed requirements and flasking

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 import pytz
 from urllib.request import urlopen
 import json
+from flask import jsonify
 
 try:
     firebase_admin.get_app()
@@ -27,18 +28,28 @@ def process_event(event):
         now = datetime.now(pytz.utc).astimezone(registration_start.tzinfo)
         if now <= registration_start and now >= notification_time - timedelta(days=1):
             send_fcm_notification(title, registration_start_str, event_type)
-        # else:
-        #     print(title + " " + str(registration_start))
+    
 
-def daily_event_checker():
+
+def daily_event_checker(request):
     base_url = "https://old.online.ntnu.no/api/v1/events/?page="
     pages = [1, 2] 
+    all_events_processed = True
 
     for page in pages:
         page_url = base_url + str(page)
-        events = fetch_events(page_url)
-        for event in events:
-            process_event(event)
+        try:
+            events = fetch_events(page_url)
+            for event in events:
+                process_event(event)
+        except Exception as e:
+            print(f"Failed to process events from {page_url}: {e}")
+            all_events_processed = False
+
+    if all_events_processed:
+        return jsonify({"message": "All events processed successfully"}), 200
+    else:
+        return jsonify({"message": "Some events could not be processed correctly"}),
 
 def send_fcm_notification(title, registration_start_str, event_type):
     registration_start = datetime.strptime(registration_start_str, "%Y-%m-%dT%H:%M:%S%z")
@@ -56,7 +67,9 @@ def send_fcm_notification(title, registration_start_str, event_type):
 
     try:
         response = messaging.send(message)
-        print(f'Successfully sent message: {response}')
+        return f'Successfully sent message: {response}'
     except Exception as e:
-        print(f'Error sending message: {e}')
-        
+        return f'Error sending message: {e}'
+    
+
+daily_event_checker('s')

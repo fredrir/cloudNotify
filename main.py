fbd5c8dbbe29f90ff92972fd06fc9ecd96155b56
fredrir <fhansteen@gmail.com>
2024-04-08 17:40:22 +0200

    context fix

--- main.py
+++ main.py
@@ -11,22 +11,34 @@ except ValueError as e:
     cred = credentials.ApplicationDefault()
     firebase_admin.initialize_app(cred)
 
-def daily_event_checker(event, context):
-    url = "https://old.online.ntnu.no/api/v1/events/"
-    response = urlopen(url)
+def fetch_events(page_url):
+    response = urlopen(page_url)
     data_json = json.loads(response.read())
-    
-    for event in data_json['results']:
-        if event['attendance_event'] is not None:
-            event_type = event['event_type']  
-            registration_start_str = event['attendance_event']['registration_start']
-            title = event['title']
-            registration_start = datetime.strptime(registration_start_str, "%Y-%m-%dT%H:%M:%S%z")
-            notification_time = registration_start - timedelta(minutes=15)
-            
-            now = datetime.now(pytz.timezone(registration_start.tzinfo.zone))
-            if now <= registration_start and now >= notification_time - timedelta(days=1):
-                send_fcm_notification(title, registration_start_str, event_type)  
+    return data_json['results']
+
+def process_event(event):
+    if event['attendance_event'] is not None:
+        event_type = event['event_type']
+        registration_start_str = event['attendance_event']['registration_start']
+        title = event['title']
+        registration_start = datetime.strptime(registration_start_str, "%Y-%m-%dT%H:%M:%S%z")
+        notification_time = registration_start - timedelta(minutes=15)
+        
+        now = datetime.now(pytz.utc).astimezone(registration_start.tzinfo)
+        if now <= registration_start and now >= notification_time - timedelta(days=1):
+            send_fcm_notification(title, registration_start_str, event_type)
+        # else:
+        #     print(title + " " + str(registration_start))
+
+def daily_event_checker():
+    base_url = "https://old.online.ntnu.no/api/v1/events/?page="
+    pages = [1, 2] 
+
+    for page in pages:
+        page_url = base_url + str(page)
+        events = fetch_events(page_url)
+        for event in events:
+            process_event(event)
 
 def send_fcm_notification(title, registration_start_str, event_type):
     registration_start = datetime.strptime(registration_start_str, "%Y-%m-%dT%H:%M:%S%z")
@@ -47,19 +59,4 @@ def send_fcm_notification(title, registration_start_str, event_type):
         print(f'Successfully sent message: {response}')
     except Exception as e:
         print(f'Error sending message: {e}')
-
-
-def send_test_notification(request):
-    message = messaging.Message(
-        notification=messaging.Notification(
-            title='Test',
-            body='Test notifikasjon'
-        ),
-        topic='allUsers'
-    )
-
-    try:
-        response = messaging.send(message)
-        return f'Successfully sent message: {response}'
-    except Exception as e:
-        return f'Error sending message: {e}'
+        
